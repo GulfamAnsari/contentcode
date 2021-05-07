e8cb591ae8363d7006aa6560a141389bf5a1b917
Gulfam Ansari <gulfam.ansari@paytm.com>
2021-05-07 21:26:04 +0530

    a

--- head.php
+++ head.php
@@ -1,10 +1,10 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=Edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<link rel="stylesheet" href="css/bootstrap.min.css">
-<link rel="stylesheet" href="css/font-awesome.min.css">
-<link rel="stylesheet" href="css/aos.css">
-<link rel="stylesheet" href="css/owl.carousel.min.css">
-<link rel="stylesheet" href="css/owl.theme.default.min.css">
+<link rel="stylesheet" href="/css/bootstrap.min.css">
+<link rel="stylesheet" href="/css/font-awesome.min.css">
+<link rel="stylesheet" href="/css/aos.css">
+<link rel="stylesheet" href="/css/owl.carousel.min.css">
+<link rel="stylesheet" href="/css/owl.theme.default.min.css">
 <!-- MAIN CSS -->
-<link rel="stylesheet" href="css/templatemo-digital-trend.css">
+<link rel="stylesheet" href="/css/templatemo-digital-trend.css">
