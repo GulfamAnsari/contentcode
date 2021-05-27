c2571d0a389f220a0303bf3668e2f9ac68806aa9
Gulfam Ansari <gulfam.ansari@paytm.com>
2021-05-28 02:34:54 +0530

    a

--- contact/index.php
+++ contact/index.php
@@ -57,7 +57,7 @@ https://templatemo.com/tm-538-digital-trend
                     
                     <!-- Follow https://templatemo.com/contact page to setup your own contact form -->
                     
-                      <form action="" onsubmit="sendEmail()" method="post" class="contact-form" data-aos="fade-up" data-aos-delay="300" role="form">
+                      <form onsubmit="sendEmail(e)"  action="" method="post" class="contact-form" data-aos="fade-up" data-aos-delay="300" role="form">
                         <div class="row">
                           <div class="col-lg-6 col-12">
                             <input type="text" id="name" class="form-control" name="name" placeholder="Name">
@@ -72,7 +72,7 @@ https://templatemo.com/tm-538-digital-trend
                           </div>
 
                           <div class="col-lg-5 mx-auto col-7">
-                            <button class="form-control" id="submit">Send Message</button>
+                            <button type="submit" class="form-control" id="submit-button" name="submit">Send Message</button>
                           </div>
                           <div class="col-lg-5 mx-auto col-7">
                             <p style="color: green" id="success"></p>
@@ -101,7 +101,8 @@ https://templatemo.com/tm-538-digital-trend
      
 
 <script>
-    function sendEmail() {
+    function sendEmail(e) {
+      e.preventDefault()
         var body = { name, email, "sender": "contentcode", "subject": "Subject", message };
         // API for get requests
         let fetchRes = fetch(
