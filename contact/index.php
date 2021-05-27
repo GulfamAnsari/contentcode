4b5616340ac4e2c728edd5f36b6340ee7533c20a
Gulfam Ansari <gulfam.ansari@paytm.com>
2021-05-28 02:37:33 +0530

    a

--- contact/index.php
+++ contact/index.php
@@ -72,7 +72,7 @@ https://templatemo.com/tm-538-digital-trend
                           </div>
 
                           <div class="col-lg-5 mx-auto col-7">
-                            <button type="submit" class="form-control" id="submit-button" name="submit">Send Message</button>
+                            <button class="form-control" >Send Message</button>
                           </div>
                           <div class="col-lg-5 mx-auto col-7">
                             <p style="color: green" id="success"></p>
